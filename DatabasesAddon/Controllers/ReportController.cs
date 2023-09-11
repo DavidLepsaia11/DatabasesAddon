9e200d24a8fad5436376317e3bc3e50327bcc46d
David Lepsaia <davidlepsaia1121@gmail.com>
2023-09-11 12:34:20 +0400

    Connected  to hanadb

--- DatabasesAddon/Controllers/ReportController.cs
+++ DatabasesAddon/Controllers/ReportController.cs
@@ -1,6 +1,7 @@
-﻿using DatabasesAddon.Helpers;
+﻿using System;
+using DatabasesAddon.Helpers;
+using Sap.Data.Hana;
 using SAPbouiCOM;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,19 +25,56 @@ namespace DatabasesAddon
 
         public void FillReportGrid()
         {
-            string filePath = @"C:\Users\dlepsaia\source\repos\DatabasesAddon\DatabasesAddon\AddonFile\QueryFile.txt";
-            string query = "";
-            // "select  top 10 * from [@RSM_IC_DBLIST1] "
+            //string filePath = @"C:\Users\dlepsaia\source\repos\DatabasesAddon\DatabasesAddon\AddonFile\QueryFile.txt";
+            string query = "Select top 10  \"TransId\" from \"OJDT\"";
+            //string server = "10.132.10.104:30015";
+            //string user = "SYSTEM";
+            //string password = "GusSERta1";
+            string connectionString = "Server=10.132.10.104:30015;UserName=SYSTEM;Password=GusSERta1;CS=TSINANDALIREAL";
+
             try
             {
-                using (StreamReader reader = new StreamReader(filePath))
-                {
-                    query = reader.ReadToEnd();
-                }
 
-                ReportGrid.Item.Enabled = false;
-                ReportGrid.DataTable.ExecuteQuery(HanaTranslator.QueryHanaTransalte(query));
-                ReportGrid.AutoResizeColumns();
+                using (var hanaConnection = new HanaConnection(connectionString))
+                {
+                    hanaConnection.Open();
+                    //using (StreamReader reader = new StreamReader(filePath))
+                    //{
+                    //    query = reader.ReadToEnd();
+                    //}
+                    using (var cmd = new HanaCommand(query, hanaConnection))
+                    {
+                        using (var reader = cmd.ExecuteReader()) 
+                        {
+                            // Print column names
+                            var sbCol = new System.Text.StringBuilder();
+                            for (var i = 0; i < reader.FieldCount; i++)
+                            {
+                                if (i > 0)
+                                {
+                                    sbCol.Append(", ");
+                                }
+                                sbCol.Append(reader.GetName(i));
+                            }
+                             var rame = sbCol.ToString();
+
+                            // Print rows
+                            while (reader.Read())
+                            {
+                                var sbRow = new System.Text.StringBuilder();
+                                for (var i = 0; i < reader.FieldCount; i++)
+                                {
+                                    if (i > 0)
+                                    {
+                                        sbRow.Append(", ");
+                                    }
+                                    sbRow.Append(reader[i]);
+                                }
+                                Console.WriteLine(sbRow.ToString());
+                            }
+                        }
+                    }
+                }
 
 
             }

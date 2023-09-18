ad95f7372626af8f3b18f1c37a4ce7a10100896e
David Lepsaia <davidlepsaia1121@gmail.com>
2023-09-18 19:09:19 +0400

    Added some changes

--- DatabasesAddon/Form1.Designer.cs
+++ DatabasesAddon/Form1.Designer.cs
@@ -35,10 +35,13 @@ namespace DatabasesAddon
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(1264, 561);
             this.dataGridView1.TabIndex = 0;
             // 
